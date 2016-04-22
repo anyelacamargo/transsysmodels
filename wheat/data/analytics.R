eb98ac8650b8780ffbd78c108a8f1127cadcde04
Jan T Kim <jttkim@gmail.com>
2016-04-22 21:33:48 +0100

    fix quotes, spaces, assignment operators etc

--- wheat/data/analytics.R
+++ wheat/data/analytics.R
@@ -1,6 +1,6 @@
 rm(list=ls())
-library('affy')   # Affymetrix pre-processing
-library('limma') 
+library("affy")   # Affymetrix pre-processing
+library("limma") 
 #library(som);
 
 
@@ -13,17 +13,17 @@ softwarebin = function()
   db = c();
   for(fname in names)
   {
-    dset = read.table(fname, header=T, sep='\t');
+    dset = read.table(fname, header = TRUE, sep = "\t");
     r = mean(dset$ArrayWoRx.S_Cy5)/mean(dset$ArrayWoRx.S_Cy3); # ratio cy5/cy3 (red/green)
     
     dcy5 = sapply(dset$ArrayWoRx.S_Cy5, function(x) x/r); # Normalise using ratio
     i = intersect(which(metadata$FileName == fname), 
-                  which(metadata$Parameter.Value..Label.used..1 == 'Cy5')); # Search arrays 
+                  which(metadata$Parameter.Value..Label.used..1 == "Cy5")); # Search arrays 
     tname = append(tname, as.vector(metadata$Factor.Value..time.[i]));
     
     dcy3 = sapply(dset$ArrayWoRx.S_Cy3, function(x) x/r);
     i = intersect(which(metadata$FileName == fname), 
-                  which(metadata$Parameter.Value..Label.used..1 == 'Cy3'));
+                  which(metadata$Parameter.Value..Label.used..1 == "Cy3"));
     
     tname = append(tname, as.vector(metadata$Factor.Value..time.[i]));
     db = cbind(db, dcy5, dcy3);
@@ -39,7 +39,7 @@ softwarebin = function()
   # End dataframe assembly
   
   probes = data.frame(probes, IDNAME = sapply(probes$ID, 
-                                              function(x) paste('ebi.ac.uk:MIAMExpress:Reporter:A-MEXP-556.',x,sep='' )));
+                                              function(x) paste("ebi.ac.uk:MIAMExpress:Reporter:A-MEXP-556.",x,sep = "" )));
   
   candidategenes = candidategenes[which(candidategenes$type == 1),]; # Search for candidate genes out of paper list
   # Search for candidate genes
@@ -51,26 +51,26 @@ softwarebin = function()
   }
   
   z = match(probes$IDNAME[i], d$dset.Reporter.identifier); # Search for gene names 
-  EmpiricalCandidateGenes = cbind(probe_name=probes$Name[i], d[z,]); # Create a dataset
+  EmpiricalCandidateGenes = cbind(probe_name = probes$Name[i], d[z,]); # Create a dataset
   EmpiricalCandidateGenes = merge(candidategenes, EmpiricalCandidateGenes, 
-                                  by.x = 'Probe_name', by.y='probe_name');
+                                  by.x = "Probe_name", by.y = "probe_name");
   
-  write.table(EmpiricalCandidateGenes, file = 'empirical_candidate.csv', sep=',', row.names = F); # Export data
+  write.table(EmpiricalCandidateGenes, file = "empirical_candidate.csv", sep = ",", row.names = FALSE); # Export data
   
 }
 
 
 
 # dEMO
-MeanPerCol = function(timepoint_list, data)
+MeanPerCol <- function(timepoint_list, data)
 {
-  copydata = data;
-  db1 = c();
+  copydata <- data;
+  db1 <- c();
   for(tp in timepoint_list)
   {
-    i = which(colnames(copydata) == tp);
-    ds = apply(copydata[,i], 1, mean);
-    db1 = cbind(db1, ds);
+    i <- which(colnames(copydata) == tp);
+    ds <- apply(copydata[,i], 1, mean);
+    db1 <- cbind(db1, ds);
   }
   return(db1);
 }
@@ -83,18 +83,18 @@ readEmpiricalData <- function(fname)
 plotProfile <- function(e, probeName, geneName, xname, yname)
 {
   
-  p = as.numeric(e)
-  barplot(p, main = geneName, names.arg = names(e), las = 2, col='red', xlab = xname, ylab = yname );
+  p <- as.numeric(e)
+  barplot(p, main = geneName, names.arg = names(e), las = 2, col = "red", xlab = xname, ylab = yname );
 }
 
 
 
-readData = function(names, probes, spottypes)
+readData <- function(names, probes, spottypes)
 {
   
-  RG = read.maimages(files=names, columns=list(Rf="ArrayWoRx:S_Cy5", 
-                                               Gf="ArrayWoRx:S_Cy3", Rb="ArrayWoRx:B_Cy5",
-                                               Gb="ArrayWoRx:B_Cy3"));
+  RG <- read.maimages(files = names, columns = list(Rf = "ArrayWoRx:S_Cy5", 
+                                               Gf = "ArrayWoRx:S_Cy3", Rb = "ArrayWoRx:B_Cy5",
+                                               Gb = "ArrayWoRx:B_Cy3"));
   RG$genes <- probes;
   RG$printer <- getLayout(probes);
   RG$genes$Status <- controlStatus(spottypes,RG);
@@ -102,36 +102,35 @@ readData = function(names, probes, spottypes)
 }
 
   
-normaliseData = function(data)
+normaliseData <- function(data)
 {
-  RG = data;
+  RG <- data;
   MA <- normalizeWithinArrays(RG);
   return(MA);
   
 }
 
 
-getTargets = function(metadata)
+getTargets <- function(metadata)
 {
   
-  design = list()
+  targetList <- list()
   for(dye in unique(metadata$Parameter.Value..Label.used..1))
   {
     for(an in unique(metadata$FileName))
     {
-      i = intersect(which(metadata$Parameter.Value..Label.used..1 == dye),
+      i <- intersect(which(metadata$Parameter.Value..Label.used..1 == dye),
                     which(metadata$FileName == an));
       
-      design[[dye]][an] = as.character(metadata$Factor.Value..time.[i])
+      targetList[[dye]][an] <- as.character(metadata$Factor.Value..time.[i])
       
     }    
   }
-    
-  return(data.frame(design))
+  return(data.frame(targetList))
 }
 
   
-fitModel = function(MA, design)
+fitModel <- function(MA, design)
 {
   
   fit <- lmFit(MA, design);
@@ -141,38 +140,38 @@ fitModel = function(MA, design)
 }
 
 # create model and get data
-createLM = function(names, probes, spottypes, metadata)
+createLM <- function(names, probes, spottypes, metadata)
 {
-  RG = readData(names, probes, spottypes);
-  MA = normaliseData(RG);
-  targets = getTargets(metadata);
-  design = modelMatrix(targets, ref="-5")
-  f = fitModel(MA, design);
+  RG <- readData(names, probes, spottypes);
+  MA <- normaliseData(RG);
+  targets <- getTargets(metadata);
+  design <- modelMatrix(targets, ref = "-5")
+  f <- fitModel(MA, design);
   return(f);
   
 }
 
 
 
-unzip('E-MEXP-850.raw.1.zip');
-genetable = read.table('genenames.csv', sep=',', header=T, stringsAsFactors = TRUE);
-metadata = read.table('E-MEXP-850.sdrf.txt', header=TRUE, sep='\t', stringsAsFactors = TRUE);
-probes <- read.table("Probes.txt",header=TRUE,sep="\t",as.is=TRUE);
+unzip("E-MEXP-850.raw.1.zip");
+genetable <- read.table("genenames.csv", sep = ",", header = TRUE, stringsAsFactors = TRUE);
+metadata <- read.table("E-MEXP-850.sdrf.txt", header = TRUE, sep = "\t", stringsAsFactors = TRUE);
+probes <- read.table("Probes.txt",header = TRUE, sep = "\t", as.is = TRUE);
 spottypes <- readSpotTypes("spottypes.txt");
 
 
-fit = createLM(unique(metadata$FileName), probes, spottypes, metadata);
+fit <- createLM(unique(metadata$FileName), probes, spottypes, metadata);
 topTable(fit)$Name;
-candidategenes= c('G01_o232_plate_16');
+candidategenes <- c("G01_o232_plate_16");
 
 
 # plot genes
 for(probeNameProfile in candidategenes)
 {
-  i = which(fit$genes$Name == probeNameProfile);
-  j = which(genetable$Probe_name == probeNameProfile);
+  i <- which(fit$genes$Name == probeNameProfile);
+  j <- which(genetable$Probe_name == probeNameProfile);
   plotProfile(fit$t[i,], probeNameProfile, genetable$Category_description_blasthit[j], 
-              'timepoint', 'intensity (log10)');
-  if(readline(probeNameProfile) == 'q' ) { break();}
+              "timepoint", "intensity (log10)");
+  if(readline(probeNameProfile) == "q" ) { break();}
   
 }
